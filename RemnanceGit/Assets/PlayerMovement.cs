64d27e71939994fb95d722e069790355f29060d7
Stefan McGee <mcgeekstefan@gmail.com>
2021-02-24 11:48:10 -0700

    Functional Game
    Menu, level added, basic movement (needs to be updated), win state (change to interact win state).

---
+++ RemnanceGit/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewarsForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewarsForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewarsForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+    }
+}
