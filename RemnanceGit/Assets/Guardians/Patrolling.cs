f3ccbdef84603fd640b7775dc5aa09393f4263c0
Stefan McGee <mcgeekstefan@gmail.com>
2021-03-01 18:32:57 -0700

    changing file locations
    yupp

---
+++ RemnanceGit/Assets/Guardians/Patrolling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrolling : MonoBehaviour{
+
+    public Transform[] points;
+    int current;
+    private NavMeshAgent agent;
+
+   void Start()
+   {
+       agent = GetComponent<NavMeshAgent>();
+       current = 0;
+       agent.speed = 300;
+   }
+
+   void Update()
+   {
+       agent.destination = points[current].position;
+       if (agent.transform.position == points[current].position)
+       {
+           current = (current + 1) % points.Length;
+           agent.destination = points[current].position;
+       }
+   }
+
+}      
