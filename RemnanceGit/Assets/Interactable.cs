e5710660955e8d43c32157dd484c7f749e9b2427
Stefan McGee <mcgeekstefan@gmail.com>
2021-03-01 17:37:24 -0700

    fixed cursor at end level
    cursor now shows at the level complete scene

--- RemnanceGit/Assets/Interactable.cs
+++ RemnanceGit/Assets/Interactable.cs
@@ -9,12 +9,6 @@ public class Interactable : MonoBehaviour
     public KeyCode interactKey;
     public UnityEvent interactAction;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- RemnanceGit/Assets/LevelComplete.cs
+++ RemnanceGit/Assets/LevelComplete.cs
@@ -3,7 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
-   
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
